235e61edac32af7aea4296bd02ce09400fedbb28
jinwook-song <wlsdnr129@naver.com>
2021-10-26 21:35:37 +0900

    Video URL using Regex

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -1,10 +1,7 @@
 import { BaseComponent } from "../../component.js";
 
 export class VideoComponent extends BaseComponent<HTMLElement> {
-  constructor(
-    title: string,
-    url: string = "https://www.youtube.com/embed/r8ekVHGVeRw"
-  ) {
+  constructor(title: string, url: string) {
     super(`<section class="video">
             <div class="video__player"><iframe class="video__iframe"></iframe></div>
             <h3 class="video__title"></h3>
@@ -13,12 +10,24 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     const iframe = this.element.querySelector(
       ".video__iframe"
     )! as HTMLIFrameElement;
-    iframe.src = url; // url -> videoId
+    iframe.src = this.convertToEmbededURL(url); // url -> videoId
 
     const titleElement = this.element.querySelector(
       ".video__title"
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  // using 정규표현식 Regex
+  private convertToEmbededURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
 
