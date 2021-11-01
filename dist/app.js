e1fe260ae277ef78bf5b8dadfa576c0455813068
jinwook-song <wlsdnr129@naver.com>
2021-11-01 15:36:41 +0900

    Drag & Drop Api

--- dist/app.js
+++ dist/app.js
@@ -15,6 +15,14 @@ class App {
         this.bindElementToDialog("#new-video", MediaSectionInput, (input) => new VideoComponent(input.title, input.url));
         this.bindElementToDialog("#new-note", TextSectionInput, (input) => new NoteComponent(input.title, input.body));
         this.bindElementToDialog("#new-todo", TextSectionInput, (input) => new TodoComponent(input.title, input.body));
+        this.page.addChild(new ImageComponent("Image Title", "https://picsum.photos/800/500"));
+        this.page.addChild(new VideoComponent("Video Title", "https://www.youtube.com/watch?v=ytyJgnfb40Q&ab_channel=%EC%B9%A8%EC%B0%A9%EB%A7%A8"));
+        this.page.addChild(new NoteComponent("Note Title", "Coding"));
+        this.page.addChild(new TodoComponent("Todo Title", "Typescript Course"));
+        this.page.addChild(new ImageComponent("Image Title", "https://picsum.photos/800/400"));
+        this.page.addChild(new VideoComponent("Video Title", "https://www.youtube.com/watch?v=ytyJgnfb40Q&ab_channel=%EC%B9%A8%EC%B0%A9%EB%A7%A8"));
+        this.page.addChild(new NoteComponent("Note Title", "Coding"));
+        this.page.addChild(new TodoComponent("Todo Title", "Typescript Course"));
     }
     bindElementToDialog(selector, InputComponent, makeSection) {
         const element = document.querySelector(selector);
--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,30 @@ class App {
       TextSectionInput,
       (input: TextSectionInput) => new TodoComponent(input.title, input.body)
     );
+
+    // For demo :)
+    this.page.addChild(
+      new ImageComponent("Image Title", "https://picsum.photos/800/500")
+    );
+    this.page.addChild(
+      new VideoComponent(
+        "Video Title",
+        "https://www.youtube.com/watch?v=ytyJgnfb40Q&ab_channel=%EC%B9%A8%EC%B0%A9%EB%A7%A8"
+      )
+    );
+    this.page.addChild(new NoteComponent("Note Title", "Coding"));
+    this.page.addChild(new TodoComponent("Todo Title", "Typescript Course"));
+    this.page.addChild(
+      new ImageComponent("Image Title", "https://picsum.photos/800/400")
+    );
+    this.page.addChild(
+      new VideoComponent(
+        "Video Title",
+        "https://www.youtube.com/watch?v=ytyJgnfb40Q&ab_channel=%EC%B9%A8%EC%B0%A9%EB%A7%A8"
+      )
+    );
+    this.page.addChild(new NoteComponent("Note Title", "Coding"));
+    this.page.addChild(new TodoComponent("Todo Title", "Typescript Course"));
   }
 
   private bindElementToDialog<T extends (MediaData | TextData) & Component>(
